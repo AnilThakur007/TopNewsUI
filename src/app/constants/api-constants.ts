880332e535d7a7fcb0a88654a0e1cc416a35b4e9
Anil Kumar <anilt2011@gmail.com>
2025-04-13 13:18:51 +0530

    Done few changes

--- src/app/constants/api-constants.ts
+++ src/app/constants/api-constants.ts
@@ -1,5 +1,6 @@
 export const API_CONSTANTS = {
-    BASE_URL: 'https://localhost:44338/api',
+    //BASE_URL: 'https://localhost:44338/api',
+    BASE_URL: 'https://topnews-had2fzg2djaed2a8.centralus-01.azurewebsites.net/api',
     ENDPOINTS: {
       TOP_STORIES: '/Stories/GetTopStories',
     },
